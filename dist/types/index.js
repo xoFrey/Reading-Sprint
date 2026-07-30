baf4255238e536e49e68d9eae7b62f4061c7d210
xoFrey <izel.acxr@gmail.com>
2026-07-30 12:39:37 +0200

    dist/ aus Git-Tracking entfernen

--- dist/types/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// Zentrale Typdefinitionen, die von mehreren Modulen genutzt werden.
-// Vermeidet zirkuläre Imports zwischen den Mongoose-Modellen.
-Object.defineProperty(exports, "__esModule", { value: true });
